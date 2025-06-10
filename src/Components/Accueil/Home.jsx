5fe9bb8134cf018a0185419066274dc216fdce8f
chrissergio96 <safou.christiansergio@gmail.com>
2025-06-10 18:58:40 +0100

    'ajou'

--- src/Components/Accueil/Home.jsx
+++ src/Components/Accueil/Home.jsx
@@ -9,25 +9,23 @@ import TopStories from '../TopStories/TopStories';
 
 
 const Home = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
 
+ useEffect(() => {
   AOS.init({ duration: 1000, once: true });
-
-
- 
+}, []);
 
   return (
     <div className="home-container">
-
+    <section className="meresection">
       <section className= 'plusieurs'>
         <div className='plus1'>
         <h2>À LA UNE</h2>
 
         <Carousel />
+        
 
         </div>
+        
         <div className='plus21'>
         <h2>SANTÉ</h2>
 
@@ -55,10 +53,13 @@ const Home = () => {
       </section>
 
       {/* Editorial */}
-      <section className="editorial-section" >
+      <section className="editorial-section" style={{display:'flex'}}>
+        <div>
         <h2 style={{textTransform:'uppercase'}}>Annonces</h2>
        
         <Caroussele/>
+        </div>
+     <a data-no-instant="1" href="http://www.canalplus-afrique.com/gabon" rel="noopener" class="a2t-link" target="_blank" aria-label="GABON_ACTU_300X300"><img decoding="async" src="https://gabonactu.com/wp-content/uploads/2025/06/GABON_ACTU_300X300.jpg" alt="" style={{display:'inline-block',width:'500',height:'500'}}/></a>
       </section>
 
       {/* À la Une */}
@@ -234,6 +235,8 @@ const Home = () => {
           <a href="https://www.facebook.com/RADIOTELEVISIONTOPBENDJE"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="28" viewBox="0 0 256 180"><path fill="#f00" d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134"/><path fill="#fff" d="m102.421 128.06l66.328-38.418l-66.328-38.418z"/></svg></a>
         </div>
       </footer>
+      </section>
+    
     </div>
   );
 };
