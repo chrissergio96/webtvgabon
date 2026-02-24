1037a7fc0484db8197f06b98ec26976a2f564aee
chrissergio96 <safou.christiansergio@gmail.com>
2026-02-24 11:29:36 +0100

    'erreur'

--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Home from '../Components/Accueil/Home.jsx';
 import Navbar from '../Components/Navbar/Navbar.jsx';
 import BreakingNews from '../Components/BreakingNews/BreakingNews.js';
-import Footer from '../Components/Footer/Footer.js';
 
 
 
